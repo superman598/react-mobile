0b8cb42098553444e27759186c045f8e8f25056b
Jing <1105683711@qq.com>
2020-10-13 20:50:33 +0800

    完成了按需加载和rem适配设置

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+import "./App.css"
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="app">
           
         </div>
       </Router>
---
+++ src/utils/fix.js
@@ -0,0 +1,8 @@
+function adapter() {
+  const fontSize = document.documentElement.clientWidth / 10;
+  document.documentElement.style.fontSize = fontSize + "px";
+}
+
+adapter();
+
+window.onresize = adapter;
